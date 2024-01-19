dfae216bf749ff66f47c65bac070c93c124f20fc
Christina200 <Weixiwen2001@sjtu.edu.cn>
2024-01-19 15:17:21 -0600

    new experiments on agem and mir

--- exp.sh
+++ exp.sh
@@ -18,7 +18,8 @@ CUDA_VISIBLE_DEVICES=1 nohup python main_tune.py --general config/general_1.yml 
 # MIR
 CUDA_VISIBLE_DEVICES=0 nohup python main_tune.py --general config/general_1.yml --data config/data/cifar10/cifar10_nc.yml --default config/agent/mir/mir_1k.yml --tune config/agent/mir/mir_tune.yml > mir_cifar10_tune.log
 
-CUDA_VISIBLE_DEVICES=0 nohup python general_main.py --data cifar10 --cl_type nc --agent ER --retrieve MIR --update random --mem_size 1000 --optimizer Adam --batch 4 --num_tasks 5 --fix_order True --learning_rate 0.0001 --weight_decay 0.0001 > mir_cifar10_vit_tuned.log
+CUDA_VISIBLE_DEVICES=2 nohup python general_main.py --data cifar100 --cl_type nc --agent ER --retrieve MIR --update random --mem_size 1000 --optimizer Adam --batch 4 --num_tasks 10 --fix_order True --learning_rate 0.0001 --weight_decay 0.0001 > mir_cifar100_vit_tuned.log
 
+CUDA_VISIBLE_DEVICES=0 nohup python general_main.py --data cifar10 --cl_type nc --agent ER --retrieve MIR --update random --mem_size 1000 --optimizer Adam --batch 4 --num_tasks 5 --fix_order True --learning_rate 0.0001 --weight_decay 0.0001 > mir_cifar10_vit_tuned.log
 #GDumb
 CUDA_VISIBLE_DEVICES=2 nohup python general_main.py --data cifar10 --cl_type nc --agent GDUMB --mem_size 1000 --mem_epoch 30 --minlr 0.0005 --clip 10 --optimizer Adam --batch 4 --num_tasks 5 --fix_order True --learning_rate 0.0001 --weight_decay 0.0001 > gdumb_cifar10_vit_tuned.log
